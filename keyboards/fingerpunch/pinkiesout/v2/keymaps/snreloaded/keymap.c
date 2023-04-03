b4c2046250e7549458f96021e8916057b35689a1
CrystalSpore <sporeluc@gmail.com>
2023-04-03 16:15:40 -0500

    added scroll enable flag for cirque
    also added another ctrl key for C-x. Still iffy on that
    
    Tested the right click feature for cirque, but was running into issues. TODO: debug / more testing

--- keyboards/fingerpunch/pinkiesout/v2/keymaps/snreloaded/keymap.c
+++ keyboards/fingerpunch/pinkiesout/v2/keymaps/snreloaded/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_QUOT,   KC_COMM,   KC_DOT,    KC_P,      KC_Y,      KC_LBRC,                      KC_RBRC,   KC_F,      KC_G,      KC_C,      KC_R,      KC_L,      KC_SLSH,
     MO(_MOVE),  KC_A,      KC_O,      KC_E,      KC_U,      KC_I,      KC_LBRC,                      KC_RBRC,   KC_D,      KC_H,      KC_T,      KC_N,      KC_S,      KC_MINS,
     KC_LSFT,    KC_SCLN,   KC_Q,      KC_J,      KC_K,      KC_X,      KC_LCTL,                      KC_BTN2,   KC_B,      KC_M,      KC_W,      KC_V,      KC_Z,      KC_EQL,
-                           XXXXXXX,   KC_LWIN,   KC_GRV,    KC_BSPC,   KC_DEL,   KC_MPLY, KC_MUTE,   KC_ENT,    KC_SPC,    KC_LALT,   XXXXXXX,   XXXXXXX
+                           KC_LCTL,   KC_LWIN,   KC_GRV,    KC_BSPC,   KC_DEL,   KC_MPLY, KC_MUTE,   KC_ENT,    KC_SPC,    KC_LALT,   XXXXXXX,   XXXXXXX
 ),
 
 [_MOVE] = LAYOUT_pinkiesout(
